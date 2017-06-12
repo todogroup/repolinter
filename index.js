c785976c03fce60578ee9372d2267507dab3f08c
Christine Abernathy <ca.abernathy@fb.com>
2017-06-12 14:37:28 -0700

    Rename type_exclusion.js to file_type_exclusion.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const rulesToRun = [
   require('./rules/file_existence').bind(null, {name: 'License file', files: ['LICENSE*', 'COPYING*']}),
   require('./rules/file_existence').bind(null, {name: 'Readme file', files: ['README*']}),
   require('./rules/file_contents').bind(null, {file: 'README.md', content: 'License'}),
-  require('./rules/type_exclusion').bind(null, {type: '.dll'}),
+  require('./rules/file_type_exclusion').bind(null, {type: '.dll'}),
 ]
 
 const targetDir = process.argv[2];
