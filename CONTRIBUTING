ee4af09f88e95cb94cd735e93829c0146643c4ca
Trevor Menagh <Trevor.Menagh@comcast.com>
2017-06-12 16:09:34 -0700

    Make it so we comply to our own linter.

---
+++ CONTRIBUTING
@@ -0,0 +1,1 @@
+Thank you for contributing! Create a pull request and we will review it to include in the project!
--- rules/file_starts_with.js
+++ rules/file_starts_with.js
@@ -1,3 +1,5 @@
+// Copyright 2017 TODO Group. All rights reserved.
+// Licensed under the Apache License, Version 2.0.
 
 module.exports = function(options, targetDir) {
   const fs = options.fs || require('../lib/file_system');
