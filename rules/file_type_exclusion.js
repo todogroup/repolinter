b7b51341efa28c1f8f993d021bbeb6d79eba86af
Brandon Keepers <bkeepers@github.com>
2017-06-13 11:47:44 -0700

    Manually fix remaining lint errors

--- rules/file_type_exclusion.js
+++ rules/file_type_exclusion.js
@@ -1,9 +1,6 @@
 // Copyright 2017 TODO Group. All rights reserved.
 // Licensed under the Apache License, Version 2.0.
 
-const fs = require('fs')
-const path = require('path')
-
 module.exports = function (options, targetDir) {
   const fs = options.fs || require('../lib/file_system')
   const files = fs.findAll(targetDir, options.type)
--- rules/licensee_check.js
+++ rules/licensee_check.js
@@ -1,14 +1,11 @@
 // Copyright 2017 TODO Group. All rights reserved.
 // Licensed under the Apache License, Version 2.0.
 
-const fs = require('fs')
-const path = require('path')
 const spawnSync = require('child_process').spawnSync
+const expected = '\nLicense: '
 
 module.exports = function (options, targetDir) {
-  expected = '\nLicense: '
-
-  licenseeOutput = spawnSync('licensee', [targetDir]).stdout
+  const licenseeOutput = spawnSync('licensee', [targetDir]).stdout
 
   if (licenseeOutput == null) {
     return {
