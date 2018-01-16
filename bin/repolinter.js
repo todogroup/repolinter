ed1a336ff64550dcce9e22f14e71e04227cd10fb
Roger Meier <r.meier@siemens.com>
2018-01-16 08:54:48 +0100

    chore(license): apply reuse.software best practice

--- bin/repolinter.js
+++ bin/repolinter.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 // Copyright 2017 TODO Group. All rights reserved.
-// Licensed under the Apache License, Version 2.0.
+// SPDX-License-Identifier: Apache-2.0
 
 const path = require('path')
 const repolinter = require('..')
--- rules/directory-existence.js
+++ rules/directory-existence.js
@@ -1,5 +1,5 @@
 // Copyright 2017 TODO Group. All rights reserved.
-// Licensed under the Apache License, Version 2.0.
+// SPDX-License-Identifier: Apache-2.0
 
 const fileExistence = require('./file-existence')
 module.exports = function (fileSystem, rule) {
