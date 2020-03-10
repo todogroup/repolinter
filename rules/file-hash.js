2aec6c86c9fac2a1a3b950ce96d7a4a521a4a9ee
Danno Ferrin <danno.ferrin@gmail.com>
2020-03-10 14:56:34 -0600

    Add file-hash rule
    Add a rule that checks to see if a file matches a hash.
    
    This is useful for projects that specifiy exact files, such as the Apache
    license, in a specific place with specific content.  Much better than
    making a giant glob out of the contents.
    
    Signed-off-by: Danno Ferrin <danno.ferrin@gmail.com>

---
+++ rules/file-hash.js
@@ -0,0 +1,38 @@
+// Copyright 2017 TODO Group. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+const Result = require('../lib/result')
+const crypto = require('crypto');
+
+module.exports = function (fileSystem, rule) {
+  const options = rule.options
+  const fs = options.fs || fileSystem
+  const file = fs.findFirstFile(options.file)
+
+  if (file === undefined ) {
+    const message = `not found: ${options.file}`
+    let status = options['succeed-on-non-existent']
+    if (status === undefined) {
+      status = false;
+    }
+    return [new Result(rule, message, null, status)]
+  }
+
+  let algorithm = options['algorithm'];
+  if (algorithm === undefined) {
+    algorithm = 'sha256'
+  }
+  const digester = crypto.createHash(algorithm)
+
+  let fileContents = fs.getFileContents(file)
+  if (fileContents === undefined) {
+    fileContents = ''
+  }
+  digester.update(fileContents)
+  const hash = digester.digest('hex');
+
+  const passed = hash == options['hash']
+  const message = `File ${file} ${passed ? 'matches hash' : 'doesn\'t match hash'}`
+
+  return [new Result(rule, message, file, passed)]
+}
