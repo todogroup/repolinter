2b289b3bf4c755fa7a9ba8c7de927f43a453975b
Noah Koontz <nkoontz@newrelic.com>
2020-07-13 16:09:47 -0700

    schema updates and file-create fixes

--- fixes/file-create.js
+++ fixes/file-create.js
@@ -6,6 +6,7 @@ const Result = require('../lib/result')
 const FileSystem = require('../lib/file_system')
 /** @type {any} */
 const fetch = require('node-fetch')
+const nodeFs = require('fs')
 
 /**
  * Create a new file, or replace a files contents
@@ -18,8 +19,13 @@ const fetch = require('node-fetch')
  */
 async function fileCreate (fs, options, targets, dryRun = false) {
   // check if the file exists and error if necessary
-  const exists = await fs.relativeFileExists(options.file)
-  if (options.replace && exists) { return new Result('', [{ message: `${options.file} already exists (options.replace is set to false)`, passed: false, path: options.file }], false) }
+  const exists = targets.length > 0 || (await fs.relativeFileExists(options.file))
+  if (options.replace && exists) {
+    if (targets.length > 0) {
+      return new Result('', targets.map(t => { return { passed: false, path: t, message: `${t} already exists (options.replace is set to false)` } }), false)
+    }
+    return new Result('', [{ message: `${options.file} already exists (options.replace is set to false)`, passed: false, path: options.file }], false)
+  }
 
   // read the text from the source, if necessary
   let content
@@ -36,14 +42,21 @@ async function fileCreate (fs, options, targets, dryRun = false) {
   }
   if (!content) { return new Result('Text was not specified for file-create! Did you configure the ruleset correctly?', [], false) }
 
-  // write it to the file
-  if (!dryRun) { fs.setFileContents(options.file, content) }
+  const shouldRemove = options.replace && targets.length > 0
+  if (!dryRun) {
+    // delete the old files if necessary
+    if (shouldRemove) {
+      await Promise.all(targets.map(t => nodeFs.promises.unlink(t)))
+    }
+    // write it to the file
+    fs.setFileContents(options.file, content)
+  }
 
   const message = typeof options.text === 'object'
     ? `${exists ? 'replace' : 'create'} file with text from ${options.text.file || options.text.url}`
     : `${exists ? 'replace' : 'create'} file with contents "${content}"`
 
-  return new Result('', [{ message, passed: true, path: options.file }], true)
+  return new Result(shouldRemove ? `Remove file(s) (${targets.join(', ')})` : '', [{ message, passed: true, path: options.file }], true)
 }
 
 module.exports = fileCreate
