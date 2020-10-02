0da11b9781b7ecdcb9aecdbf8fc6b10a9b3a1ee2
Noah Koontz <nkoontz@newrelic.com>
2020-10-02 10:42:29 -0700

    fix: add command-exists test to axioms for stability, moved from sync to async calls (#182)

--- tests/axioms/licensee_tests.js
+++ tests/axioms/licensee_tests.js
@@ -15,18 +15,18 @@ describe('licensee', function () {
   } else {
     const licenseeAxiom = require('../../axioms/licensee')
 
-    it('runs licensee', () => {
+    it('runs licensee', async () => {
       const mockFs = { targetDir: path.resolve(__dirname, '../../') }
-      const res = licenseeAxiom(mockFs)
+      const res = await licenseeAxiom(mockFs)
 
       expect(res.passed).to.equal(true)
       expect(res.targets).to.have.length(1)
       expect(res.targets[0].path).to.equal('Apache-2.0')
     })
 
-    it('returns nothing if no licenses are found', () => {
+    it('returns nothing if no licenses are found', async () => {
       const mockFs = { targetDir: path.resolve(__dirname) }
-      const res = licenseeAxiom(mockFs)
+      const res = await licenseeAxiom(mockFs)
 
       expect(res.passed).to.equal(true)
       expect(res.targets).to.have.length(0)
