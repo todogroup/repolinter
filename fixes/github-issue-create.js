edc1925d9c499093f437cb0f3f80f0b3f8cf40ba
Brend Smits <brend_smits@hotmail.com>
2021-03-18 08:31:03 +0100

    Setup general logic flow - Check for existing issues on repo

--- fixes/github-issue-create.js
+++ fixes/github-issue-create.js
@@ -15,21 +15,34 @@ const { Octokit } = require('@octokit/rest')
  * @returns {Promise<Result>} The fix result
  */
 async function createGithubIssue(fs, options, targets, dryRun = false) {
-  const octoKit = new Octokit({
-    auth: 'a70e431a7bc22bf169f56cb7770c8b61ab4bac81',
+  this.Octokit = new Octokit({
+    auth: 'PERSONAL ACCESS TOKEN (PAT) HERE',
     log: console,
-    baseUrl: 'https://api.github.com'
+    baseUrl: 'https://api.github.com',
+    owner: 'Brend-Smits',
+    repo: 'octokit-test-repo'
   })
+
   console.error('Github Creation Debug: ' + options)
-  try {
-    await octoKit.issues.create({
-      owner: 'Brend-Smits',
-      repo: 'octokit-test-repo',
-      title: 'test-issue'
-    })
-  } catch (e) {
-    console.error(e)
+
+  // Find issue created by Repolinter
+  const issue = await findExistingRepolinterIssues()
+
+  console.log(issue);
+  if (issue === null || issue === undefined) {
+  // Issue should include the broken rule, a message in the body and a label.
+  const createdIssue = await createIssueOnGithub()
+
+  } if(issue.state === 'closed') {
+  // We should open a single issue per rule with the role that is broken in the title
+
+    // Reopen issue, update title and comment on it to inform repo maintainers.
+    // Issue body should have a body that includes what rules have failed.
+  } else if (issue.state === 'open') {
+    // Issue is open, check body and find what rules have been broken.
+    // If the rule that has been broken, is already listed in the issue body/title, do nothing
   }
+
   // // create the issue on Github
   // if (!dryRun) {
   //   await Promise.all(targets.map(async t => fs.removeFile(t)))
@@ -40,5 +53,41 @@ async function createGithubIssue(fs, options, targets, dryRun = false) {
   })
   return new Result('', removeTargets, true)
 }
+
+async function findExistingRepolinterIssues() {
+  const issues = await this.Octokit.issues.listForRepo({
+    owner: 'Brend-Smits',
+    repo: 'octokit-test-repo',
+    labels: 'continuous-compliance',
+    creator: 'Brend-Smits'
+  })
+  if (issues.data.length === 0) {
+    return null;
+  }
+
+  const openIssues = issues.data.filter(({state}) => state === 'open')
+  if (openIssues.length > 1)
+  console.warn(
+    `Found more than one matching open issue: ${openIssues
+      .map(i => `#${i.number}`)
+      .join(', ')}. Defaulting to the most recent.`
+  )
+  return issues.data[0]
+}
+// Create issue on Github
+async function createIssueOnGithub() {
+  try {
+    return await this.Octokit.issues.create({
+      owner: 'Brend-Smits',
+      repo: 'octokit-test-repo',
+      title: 'test-issue',
+      body: 'Auto generated issue by Continuous Compliance',
+      labels: ['continuous-compliance']
+    })
+  } catch (e) {
+    console.error(e)
+  }
+}
+
 
 module.exports = createGithubIssue
