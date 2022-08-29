a69649dc63a4716befebc9b3d39a276adf9cb1f2
drgs <dragos.serban@philips.com>
2022-08-29 02:00:55 +0200

    Fix styling issues in file-or-directory-existence.js file

--- rules/file-or-directory-existence.js
+++ rules/file-or-directory-existence.js
@@ -3,28 +3,37 @@
 
 const Result = require('../lib/result')
 
-function fileOrDirectoryExistence(fs, options) {
-  const fileOrDirectoryExists = await fs.findFirst(options.globsAny, options.nocase)
+async function fileOrDirectoryExistence(fs, options) {
+  const fileOrDirectoryExists = await fs.findFirst(
+    options.globsAny,
+    options.nocase
+  )
 
   const passed = !!fileOrDirectoryExists
 
   return passed
-  ? new Result(
-      '',
-      [{ passed: true, path: file, message: 'Found file or directory matching the specified patterns' }],
-      true
-    )
-  : new Result(
-      `${
-        options['fail-message'] !== undefined
-          ? options['fail-message'] + '. '
-          : ''
-      }Did not find a file or directory matching the specified patterns`,
-      options.globsAny.map(f => {
-        return { passed: false, pattern: f }
-      }),
-      false
-    )
+    ? new Result(
+        '',
+        [
+          {
+            passed: true,
+            path: fileOrDirectoryExists,
+            message: 'Found file or directory matching the specified patterns'
+          }
+        ],
+        true
+      )
+    : new Result(
+        `${
+          options['fail-message'] !== undefined
+            ? options['fail-message'] + '. '
+            : ''
+        }Did not find a file or directory matching the specified patterns`,
+        options.globsAny.map(f => {
+          return { passed: false, pattern: f }
+        }),
+        false
+      )
 }
 
 module.exports = fileOrDirectoryExistence
