a217600f5054fe767201c0eca69ab98332e7055d
Scott Schreckengaust <scottschreckengaust@users.noreply.github.com>
2025-02-13 19:57:53 -0800

    fix: in then catch
    Signed-off-by: Scott Schreckengaust <scottschreckengaust@users.noreply.github.com>

--- tests/lib/file_system_tests.js
+++ tests/lib/file_system_tests.js
@@ -204,10 +204,13 @@ describe('lib', () => {
 
       it('should change the contents of a file', async () => {
         const expected = 'somefilecontents\nmorecontents\n'
-        await fs.setFileContents('text_file_for_test.txt', expected)
-        const fileContents = await realFs.promises.readFile(filePath, 'utf8')
-        const realFileContents = fileContents.replace(/\r/g, '')
-        expect(realFileContents).to.equal(expected)
+        fs.setFileContents('text_file_for_test.txt', expected).then(() => {
+          const fileContents = await realFs.promises.readFile(filePath, 'utf8')
+          const realFileContents = fileContents.replace(/\r/g, '')
+          expect(realFileContents).to.equal(expected)
+        }).catch(
+          //TODO: raise exception....
+        );
       })
 
       after(async () => {
