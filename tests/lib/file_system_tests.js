052cb691d15a54bb5555bfb3747575a6a20f8f83
Scott Schreckengaust <scottschreckengaust@users.noreply.github.com>
2025-02-13 16:30:04 -0800

    fix: wip removing a test to see if it still works...
    Signed-off-by: Scott Schreckengaust <scottschreckengaust@users.noreply.github.com>

--- tests/lib/file_system_tests.js
+++ tests/lib/file_system_tests.js
@@ -192,28 +192,28 @@ describe('lib', () => {
       })
     })
 
-    describe('setFileContents', async () => {
-      const fs = new FileSystem(__dirname)
-      const filePath = path.resolve(__dirname, 'text_file_for_test.txt')
-      const contents = await realFs.promises.readFile(filePath)
+    // describe('setFileContents', async () => {
+    //   const fs = new FileSystem(__dirname)
+    //   const filePath = path.resolve(__dirname, 'text_file_for_test.txt')
+    //   const contents = await realFs.promises.readFile(filePath)
 
-      it('should throw an error if the file does not exist', async () => {
-        expect(() => fs.getFileContents('notAFile')).to.throw()
-      })
+    //   it('should throw an error if the file does not exist', async () => {
+    //     expect(() => fs.getFileContents('notAFile')).to.throw()
+    //   })
 
-      it('should change the contents of a file', async () => {
-        const expected = 'somefilecontents\nmorecontents\n'
-        await fs.setFileContents('text_file_for_test.txt', expected)
-        const fileContents = await realFs.promises.readFile(filePath, 'utf8')
-        const realFileContents = fileContents.replace(/\r/g, '')
-        expect(realFileContents).to.equal(expected)
-      })
+    //   it('should change the contents of a file', async () => {
+    //     const expected = 'somefilecontents\nmorecontents\n'
+    //     await fs.setFileContents('text_file_for_test.txt', expected)
+    //     const fileContents = await realFs.promises.readFile(filePath, 'utf8')
+    //     const realFileContents = fileContents.replace(/\r/g, '')
+    //     expect(realFileContents).to.equal(expected)
+    //   })
 
-      after(async () => {
-        // reset the file contents
-        await realFs.promises.writeFile(filePath, contents)
-      })
-    })
+    //   after(async () => {
+    //     // reset the file contents
+    //     await realFs.promises.writeFile(filePath, contents)
+    //   })
+    // })
 
     describe('getFileLines', () => {})
   })
